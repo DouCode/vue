afcc521c68aef649ac7d419b361a27f5bcbf4a28
DouCode <34049216+DouCode@users.noreply.github.com>
2022-04-12 15:42:54 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
+module.exports = {
   transpileDependencies: true,
   lintOnSave: false,
-});
+  configureWebpack: {
+	    //关闭 webpack 的性能提示
+	    performance: {
+		    hints:false
+	    }
+  }
+};
